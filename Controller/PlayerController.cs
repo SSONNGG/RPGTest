e3160c768ae3409583211572fbdbdcb50b56d715
宋宋宋宋宋 <1647478489@qq.com>
2023-07-08 19:58:48 +0800

    <MISC> Controller接口定义和简单实现

--- Controller/PlayerController.cs
+++ Controller/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using RPGTest.Role;
+﻿using RPGTest.Panel;
+using RPGTest.Role;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +8,71 @@ using System.Threading.Tasks;
 
 namespace RPGTest.Controller
 {
-    //
-    internal class PlayerController : Player
+    //用户控制器，计算战斗过程
+    class PlayerController : IControllerPanel
     {
+        private List<Player> lstPlayers;    //玩家
 
+        private List<Player> lstComputers;  //电脑
+
+        public List<Player> LstPlayers 
+        { 
+            get => lstPlayers;
+            set => lstPlayers = value;
+        }
+
+        public List<Player> LstComputers
+        { 
+            get => lstComputers;
+            set => lstComputers = value;
+        }
+
+        
+        public void onStart(List<Player> player, List<Player> computer)
+        {
+            //加载角色数据
+            LstComputers = computer;
+            LstPlayers = player;
+        }
+
+
+        public void onRun()
+        {
+            while (LstComputers.Count < 0 || LstPlayers.Count < 0)
+            {
+                //执行速度条
+                onPause();
+            }
+            
+            if(onStop())
+            {
+                Console.WriteLine("Player Win!");
+            }else
+            {
+                Console.WriteLine("Computer Win!");
+            }
+        }
+
+        public void onPause()
+        {
+            //接收用户输入
+            //执行用户决策
+        }
+
+        public bool onStop()
+        {
+            if(LstComputers.Count <= 0 && lstPlayers.Count > 0)
+            {
+                LstComputers.Clear();
+                lstPlayers.Clear();
+                return true;
+            }
+            else
+            {
+                LstComputers.Clear();
+                lstPlayers.Clear();
+                return false;
+            }
+        }
     }
 }
---
+++ Panel/IControllerPanel.cs
@@ -0,0 +1,35 @@
+﻿using RPGTest.Role;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGTest.Panel
+{
+    interface IControllerPanel
+    {
+        /// <summary>
+        /// 开始，加载数据
+        /// </summary>
+        /// <param name="player">玩家角色列表</param>
+        /// <param name="computer">电脑角色列表</param>
+        public void onStart(List<Player> player,List<Player> computer);
+
+        /// <summary>
+        /// 执行，启动战斗
+        /// </summary>
+        public void onRun();
+
+        /// <summary>
+        /// 暂停，玩家决策
+        /// </summary>
+        public void onPause();
+
+        /// <summary>
+        /// 结束，判断胜负
+        /// </summary>
+        /// <returns>玩家胜利返回true<br/>电脑胜利返回false</returns>
+        public bool onStop();
+    }
+}
